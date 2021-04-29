a941bbfbdb265d47c2d92deb172bc228c559bdac
wangandi520 <wangandi520@gmail.com>
2021-04-29 16:11:02 +0800

    first version

---
+++ PNG2PDF.py
@@ -0,0 +1,27 @@
+import os
+
+def main():
+    bookNames = []
+    allFiles = os.listdir('.')
+    for fileName in allFiles:
+        if os.path.isdir(fileName):
+            bookNames.append(fileName)
+    bookDir = []
+    bookPNGs = []
+    for eachBookName in bookNames:
+        PNGNames = os.listdir(eachBookName)
+        bookPNGs.append(PNGNames)
+    
+    cmds = []
+    for i in range(0, len(bookNames)):
+        tmp = 'mutool.exe convert -o ' + bookNames[i] + '.pdf '
+        for j in range(0, len(bookPNGs[i])):
+            #print(bookPNGs[i][j])
+            tmp = tmp + str(bookNames[i]) + '\\' + str(bookPNGs[i][j] + ' ')
+        cmds.append(tmp)
+    
+    for cmd in cmds:
+        os.system(cmd)
+    
+if __name__ == '__main__':
+    main()
---
+++ README.md
@@ -0,0 +1,13 @@
+## 使用mupdf批量转换png到pdf
+
+### 目录结构
+
+把PNG2PDF.py和数个文件夹（每个文件夹相当于一本书，如XX第一卷）放在同级目录，这些文件夹里面是png文件。
+
+---
+
+## using mupdf, convert PNGs to PDF
+
+### folder constructure
+
+put PNG2PDF.py and many folders(each folder = a book) in a same folder, each folder contents PNGs
