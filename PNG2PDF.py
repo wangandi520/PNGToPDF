c5d3de9c4ec685dfef613eef3be3a782e330e714
wangandi520 <wangandi520@gmail.com>
2021-05-02 09:16:33 +0800

    improve output message

--- PNG2PDF.py
+++ PNG2PDF.py
@@ -1,4 +1,5 @@
 import os
+import time
 
 def main():
     bookNames = []
@@ -12,16 +13,18 @@ def main():
             bookDirs.append(bookName + '\\' + os.listdir(bookName)[0])
         else:
             bookDirs.append(bookName)
-    print('Found ' + str(len(bookNames)) + ' books:')
+    print('Found ' + str(len(bookNames)) + ' book(s):')
     for bookName in bookNames:
         print('  ' + bookName)
     if not os.path.exists('PDF'):
         os.system('mkdir PDF')
     for bookName,bookDir in zip(bookNames,bookDirs):
         #print(bookDir)
+        startTime = time.time()
         print('Converting PNG to PDF: ' + bookName + '...')
         os.system('convert "' + bookDir + '\\*.{png,jpeg,jpg}" -quality 100 PDF\\' + bookName + '.pdf')
-        print('  Completed.')
+        endTime = time.time()
+        print('  Completed. Converted %d file(s) in %d second(s).' % (len(os.listdir(bookDir)) , (endTime - startTime)))
         
 if __name__ == '__main__':
     main()
