b401cd396ad7aebf111fe0b33f857e78fe67db2a
wangandi520 <wangandi520@gmail.com>
2021-05-02 12:09:30 +0800

    support drag folder

--- PNG2PDF.py
+++ PNG2PDF.py
@@ -1,30 +1,39 @@
+# encoding:utf-8
+
 import os
 import time
+import sys
 
-def main():
+def main(localDir):
     bookNames = []
     bookDirs = []
-    allFiles = os.listdir('.')
+    allFiles = os.listdir(localDir)
     for fileName in allFiles:
-        if os.path.isdir(fileName) and fileName != 'PDF':
+        if os.path.isdir(os.path.join(localDir, fileName)) and fileName != 'PDF':
             bookNames.append(fileName)
     for bookName in bookNames:
-        if (os.path.isdir(bookName) and len(os.listdir(bookName)) == 1):
-            bookDirs.append(bookName + '\\' + os.listdir(bookName)[0])
+        subDir = os.listdir(os.path.join(localDir, bookName))
+        allDir = os.path.join(localDir, bookName, subDir[0])
+        if (os.path.isdir(os.path.join(localDir, bookName, subDir[0])) and len(subDir) == 1):
+            bookDirs.append(os.path.join(localDir, bookName, subDir[0]))
         else:
-            bookDirs.append(bookName)
+            bookDirs.append(os.path.join(localDir, bookName))
+    print('In ' + localDir)
     print('Found ' + str(len(bookNames)) + ' book(s):')
     for bookName in bookNames:
         print('  ' + bookName)
-    if not os.path.exists('PDF'):
-        os.system('mkdir PDF')
+    if not os.path.exists(os.path.join(localDir,'PDF')):
+        os.system('mkdir ' + os.path.join(localDir,'PDF'))
     for bookName,bookDir in zip(bookNames,bookDirs):
-        #print(bookDir)
         startTime = time.time()
         print('Converting : ' + bookName + '...')
-        os.system('convert "' + bookDir + '\\*.{png,jpeg,jpg}" -quality 100 PDF\\' + bookName + '.pdf')
+        inputCmd = 'convert.exe "' + bookDir + '\\*.{png,jpeg,jpg}" -quality 100 "' + localDir + '\\PDF\\' + bookName + '.pdf"'
+        os.system(inputCmd)
         endTime = time.time()
         print('  Completed. Converted %d file(s) in %d second(s).' % (len(os.listdir(bookDir)) , (endTime - startTime)))
         
 if __name__ == '__main__':
-    main()
+    if len(sys.argv) == 1:
+        main('.')
+    elif len(sys.argv) == 2:
+        main(sys.argv[1])
--- README.md
+++ README.md
@@ -4,6 +4,8 @@
 
 把PNG2PDF.py和数个文件夹（每个文件夹相当于一本书，如XX第一卷）放在同级目录，这些文件夹里面是png文件。输出到PDF文件夹里。
 
+可以拖拽文件夹到py文件，注意，这个文件夹是包含数个文件夹（数本书）的上级文件夹（如XX作品合集）
+
 ---
 
 
@@ -12,6 +14,8 @@
 ### PNG to PDF
 
 put PNG2PDF.py and many folders(each folder = a book) in a same folder, each folder contents PNGs, output in PDF folder.
+
+support drag folder to py, the folder contains many folders(many books)
 
 ---
 
